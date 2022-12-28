186bba8af50139a52b19e14df3895350f3185fa9
sborel492 <114200272+sborel492@users.noreply.github.com>
2022-12-27 19:05:31 -0600

    Update MealCraftDao.java
    Removed unused assets

--- mealcraft/src/main/java/com/aca/mealcraft/dao/MealCraftDao.java
+++ mealcraft/src/main/java/com/aca/mealcraft/dao/MealCraftDao.java
@@ -8,7 +8,6 @@ import com.aca.mealcraft.model.Recipe;
 public interface MealCraftDao {
 	public List<Recipe> getRecipes();
 	public List<Recipe> getRecipesByCategory(String category);
-//	public List<Recipe> getRecipesByIngredient(String ingredient);
 	public List<Recipe> getRecipeById(Integer recipeId);
 	public List<Recipe> getRecipesByName(String name);
 	public Recipe createRecipe(Recipe newRecipe);
